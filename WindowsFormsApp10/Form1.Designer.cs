2a6d824b61775839fa1c1e80abd01aa056116a24
Gregor Schulte <gregor.schulte@uni-tuebingen.de>
2017-09-19 13:01:46 +0200

    Add project files.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MyTextBox = new System.Windows.Forms.TextBox();
+            this.MyProgressbar = new System.Windows.Forms.ProgressBar();
+            this.MyPictureBox_2 = new System.Windows.Forms.PictureBox();
+            this.MyLabelScore = new System.Windows.Forms.Label();
+            this.MyFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.MyLabelFilename = new System.Windows.Forms.Label();
+            this.MyLabelPosition = new System.Windows.Forms.Label();
+            this.MyLabelMaximum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MyPictureBox_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MyTextBox
+            // 
+            this.MyTextBox.AcceptsReturn = true;
+            this.MyTextBox.AcceptsTab = true;
+            this.MyTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.MyTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.MyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MyTextBox.Enabled = false;
+            this.MyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyTextBox.ForeColor = System.Drawing.Color.White;
+            this.MyTextBox.Location = new System.Drawing.Point(73, 29);
+            this.MyTextBox.Margin = new System.Windows.Forms.Padding(100);
+            this.MyTextBox.Multiline = true;
+            this.MyTextBox.Name = "MyTextBox";
+            this.MyTextBox.ReadOnly = true;
+            this.MyTextBox.Size = new System.Drawing.Size(473, 229);
+            this.MyTextBox.TabIndex = 1;
+            this.MyTextBox.Text = resources.GetString("MyTextBox.Text");
+            // 
+            // MyProgressbar
+            // 
+            this.MyProgressbar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MyProgressbar.Location = new System.Drawing.Point(12, 609);
+            this.MyProgressbar.Name = "MyProgressbar";
+            this.MyProgressbar.Size = new System.Drawing.Size(624, 23);
+            this.MyProgressbar.Step = 100;
+            this.MyProgressbar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.MyProgressbar.TabIndex = 1;
+            // 
+            // MyPictureBox_2
+            // 
+            this.MyPictureBox_2.BackColor = System.Drawing.Color.Black;
+            this.MyPictureBox_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.MyPictureBox_2.Location = new System.Drawing.Point(-1, 0);
+            this.MyPictureBox_2.Name = "MyPictureBox_2";
+            this.MyPictureBox_2.Size = new System.Drawing.Size(586, 477);
+            this.MyPictureBox_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MyPictureBox_2.TabIndex = 4;
+            this.MyPictureBox_2.TabStop = false;
+            this.MyPictureBox_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MyMouseDown);
+            this.MyPictureBox_2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MyMouseMove);
+            this.MyPictureBox_2.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.MyMouseWheel);
+            // 
+            // MyLabelScore
+            // 
+            this.MyLabelScore.AutoSize = true;
+            this.MyLabelScore.BackColor = System.Drawing.Color.Transparent;
+            this.MyLabelScore.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MyLabelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyLabelScore.ForeColor = System.Drawing.Color.Yellow;
+            this.MyLabelScore.Location = new System.Drawing.Point(591, 0);
+            this.MyLabelScore.Name = "MyLabelScore";
+            this.MyLabelScore.Size = new System.Drawing.Size(57, 55);
+            this.MyLabelScore.TabIndex = 5;
+            this.MyLabelScore.Text = "X";
+            this.MyLabelScore.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MyLabelFilename
+            // 
+            this.MyLabelFilename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MyLabelFilename.AutoSize = true;
+            this.MyLabelFilename.BackColor = System.Drawing.Color.Transparent;
+            this.MyLabelFilename.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyLabelFilename.ForeColor = System.Drawing.Color.Yellow;
+            this.MyLabelFilename.Location = new System.Drawing.Point(12, 573);
+            this.MyLabelFilename.Name = "MyLabelFilename";
+            this.MyLabelFilename.Size = new System.Drawing.Size(219, 33);
+            this.MyLabelFilename.TabIndex = 6;
+            this.MyLabelFilename.Text = "##filename##";
+            this.MyLabelFilename.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MyLabelPosition
+            // 
+            this.MyLabelPosition.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MyLabelPosition.AutoSize = true;
+            this.MyLabelPosition.BackColor = System.Drawing.Color.Transparent;
+            this.MyLabelPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyLabelPosition.ForeColor = System.Drawing.Color.Yellow;
+            this.MyLabelPosition.Location = new System.Drawing.Point(12, 540);
+            this.MyLabelPosition.Name = "MyLabelPosition";
+            this.MyLabelPosition.Size = new System.Drawing.Size(192, 33);
+            this.MyLabelPosition.TabIndex = 7;
+            this.MyLabelPosition.Text = "##position##";
+            this.MyLabelPosition.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MyLabelMaximum
+            // 
+            this.MyLabelMaximum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.MyLabelMaximum.AutoSize = true;
+            this.MyLabelMaximum.BackColor = System.Drawing.Color.Transparent;
+            this.MyLabelMaximum.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyLabelMaximum.ForeColor = System.Drawing.Color.Yellow;
+            this.MyLabelMaximum.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.MyLabelMaximum.Location = new System.Drawing.Point(417, 540);
+            this.MyLabelMaximum.Name = "MyLabelMaximum";
+            this.MyLabelMaximum.Size = new System.Drawing.Size(219, 33);
+            this.MyLabelMaximum.TabIndex = 8;
+            this.MyLabelMaximum.Text = "##maximum##";
+            this.MyLabelMaximum.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(648, 644);
+            this.Controls.Add(this.MyLabelMaximum);
+            this.Controls.Add(this.MyLabelPosition);
+            this.Controls.Add(this.MyLabelFilename);
+            this.Controls.Add(this.MyLabelScore);
+            this.Controls.Add(this.MyProgressbar);
+            this.Controls.Add(this.MyTextBox);
+            this.Controls.Add(this.MyPictureBox_2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyKeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.MyPictureBox_2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox MyTextBox;
+        private System.Windows.Forms.ProgressBar MyProgressbar;
+        private System.Windows.Forms.PictureBox MyPictureBox_2;
+        private System.Windows.Forms.Label MyLabelScore;
+        private System.Windows.Forms.FolderBrowserDialog MyFolderBrowserDialog;
+        private System.Windows.Forms.Label MyLabelFilename;
+        private System.Windows.Forms.Label MyLabelPosition;
+        private System.Windows.Forms.Label MyLabelMaximum;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+
+        private void MakeDataTableAndDisplay(string MyFilename)
+        {
+            string fileName = MyFilename;
+            // Create new DataTable.
+            DataTable MyTable = new DataTable();
+
+            // Declare DataColumn and DataRow variables.
+            DataColumn column;
+            DataRow row;
+
+            // Create new DataColumn, set DataType, ColumnName
+            // and add to DataTable.    
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "id";
+            MyTable.Columns.Add(column);
+
+            // Create second column.
+            column = new DataColumn();
+            column.DataType = Type.GetType("System.String");
+            column.ColumnName = "filename";
+            MyTable.Columns.Add(column);
+
+            // Create thrid column.
+            column = new DataColumn();
+            column.DataType = Type.GetType("System.Int32");
+            column.ColumnName = "score";
+            MyTable.Columns.Add(column);
+
+            // Create new DataRow objects and add to DataTable.    
+            for (int i = 0; i < 10; i++)
+            {
+                row = MyTable.NewRow();
+                row["id"] = i;
+                row["filename"] = fileName;
+                row["score"] = 0;
+                MyTable.Rows.Add(row);
+            }
+            // Set to DataGrid.DataSource property to the table.
+            //dataGrid1.DataSource = table;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MyTextBox.Visible = false;
+            MyLabelFilename.Visible =false;
+            MyLabelMaximum.Visible = false;
+            MyLabelPosition.Visible =false;
+            MyProgressbar.Visible = false;
+
+            MyPictureBox_2.Height = this.Height;
+            MyPictureBox_2.Width = this.Width;
+        }
+
+        int MyMaximum = 0;
+        private int MyPosition = 0;
+        int[] score;
+        string[] files;
+        public string MySourceFolder = "empty";
+        public String[] filters = new String[] { "jpg", "jpeg" };
+        string[] mytable;
+        private Point mouseDown;
+
+        private void MyKeyPress(object sender, KeyEventArgs e)
+        {
+            if ((ModifierKeys & Keys.Shift) == Keys.Shift)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Right:
+               
+                    next100Images();
+                        break;
+                    case Keys.Left:
+                        previous100Images();
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+            else
+            {
+                switch (e.KeyCode)
+                {
+
+                    case Keys.H:
+                        MyTextBox.Visible = !MyTextBox.Visible;
+                        break;
+
+                    case Keys.Escape:
+                        Application.Exit();
+                        break;
+
+                    case Keys.Right:
+                        nextImage();
+                        break;
+
+                    case Keys.Left:
+                         previousImage(); 
+                        break;
+                    case Keys.NumPad1:
+                        if (score !=null)
+                        { 
+                            score[MyPosition] = 1;
+                            MyLabelScore.Text = score[MyPosition].ToString();
+                            nextImage();
+                        }
+                        else { Console.Beep(); }
+                        break;
+
+                    case Keys.NumPad2:
+                        if (score != null)
+                        {
+                            score[MyPosition] = 2;
+                            MyLabelScore.Text = score[MyPosition].ToString();
+                            nextImage();
+                        }
+                        else { Console.Beep(); }
+                        break;
+
+                    case Keys.NumPad3:
+                        if (score != null)
+                        {
+                            score[MyPosition] =3;
+                            MyLabelScore.Text = score[MyPosition].ToString();
+                            nextImage();
+                        }
+                        else { Console.Beep(); }
+                        break;
+
+                    case Keys.NumPad4:
+                        if (score != null)
+                        {
+                            score[MyPosition] = 4;
+                            MyLabelScore.Text = score[MyPosition].ToString();
+                            nextImage();
+                        }
+                        else { Console.Beep(); }
+                        break;
+
+                    case Keys.NumPad5:
+                        if (score != null)
+                        {
+                            score[MyPosition] = 5;
+                            MyLabelScore.Text = score[MyPosition].ToString();
+                            nextImage();
+                        }
+                        else { Console.Beep(); }                    
+                        break;
+
+                    case Keys.O:
+                        if (MyFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            MyPosition = 0;
+                            if (files != null)
+                            {
+                                Array.Clear(files, 0, files.Length);
+                                Console.Beep();
+                            }
+                            //Array.Clear(score, 0, score.Length);
+                            MySourceFolder = MyFolderBrowserDialog.SelectedPath;
+                            files = GetFilesFrom(MySourceFolder, filters, false);
+                            if (Path.GetFileNameWithoutExtension(files[MyPosition]).Length==54)
+                                { 
+                            Array.Sort(files, (a, b) => string.Compare(Path.GetFileNameWithoutExtension(a).Substring(46,8), Path.GetFileNameWithoutExtension(b).Substring(46, 8))) ;
+                            //MyPosMax = files.Length;
+                            //MyLabelScore.Text = MyPosMax.ToString();
+                            MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                            MyMaximum = files.Length;
+                            //MyLabelScore.Text = Path.GetFileNameWithoutExtension(files[1]).Substring(46, 8);// files[1].Substring(55, 8).ToString();
+
+                            score = new int[files.Length];
+
+                            MyLabelScore.Text = score[MyPosition].ToString();
+
+                            MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                            MyProgressbar.Maximum = files.Length-1;
+                        
+                            MyProgressbar.Value = MyPosition;
+                            MyLabelPosition.Text = MyPosition.ToString();
+                            MyLabelMaximum.Text = MyMaximum.ToString();
+
+                            }
+                            else MessageBox.Show("Filenames do not match expected scheme");
+
+                        }
+                        break;
+
+                    case Keys.W:
+                        writeResults();
+                        break;
+
+                    case Keys.J:
+                        MyLabelFilename.Visible = !MyLabelFilename.Visible;
+                        MyLabelPosition.Visible = !MyLabelPosition.Visible;
+                        MyProgressbar.Visible = !MyProgressbar.Visible;
+                        MyLabelMaximum.Visible = !MyLabelMaximum.Visible;
+                        break;
+
+                    case Keys.F:                    
+                        MyPictureBox_2.Width = this.Width;
+                        MyPictureBox_2.Height = this.Height;
+                        MyPictureBox_2.Location = new Point(0, 0);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+
+            Invalidate();
+        }
+
+        private void MyMouseDown(object sender, EventArgs e)
+        {
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            if (mouse.Button == MouseButtons.Left)
+            {
+                 mouseDown = mouse.Location;
+            }
+        }
+
+        private void MyMouseWheel(object sender, MouseEventArgs e)
+        {
+
+            if (e.Delta > 0)
+            {
+                if (MyPictureBox_2.Width < 4000)
+                {
+                    //MyPictureBox_2.Location = new Point(MyPictureBox.Location.X - (int)(MyPictureBox_2.Location.X * 0.075), MyPictureBox_2.Location.Y - (int)(MyPictureBox_2.Location.Y * 0.075));
+
+                    MyPictureBox_2.Width = (int)(MyPictureBox_2.Width * 1.1);
+                    MyPictureBox_2.Height = (int)(MyPictureBox_2.Width * 1.1);
+                }
+            }
+            else
+            {
+                if (MyPictureBox_2.Width > 100)
+                {
+                    //MyPictureBox_2.Location = new Point(MyPictureBox.Location.X + (int)(MyPictureBox_2.Location.X * 0.075), MyPictureBox_2.Location.Y + (int)(MyPictureBox_2.Location.Y * 0.075));
+
+                    MyPictureBox_2.Width = (int)(MyPictureBox_2.Width * 0.9);
+                    MyPictureBox_2.Height = (int)(MyPictureBox_2.Width * 0.9);
+                }
+            }
+            
+        }
+
+        private void MyMouseMove(object sender, EventArgs e)
+        {
+            //MouseDown = mouse.Location;
+            MouseEventArgs mouse = e as MouseEventArgs;            
+           
+            if (mouse.Button == MouseButtons.Left)
+            {
+                Point mousePosNow = mouse.Location;
+                //MyTextBox.Text = "X: "+ mouseDown.X.ToString() +" Y: "+ mouseDown.Y.ToString();
+
+                int deltaX = mousePosNow.X - mouseDown.X;
+                int deltaY = mousePosNow.Y - mouseDown.Y;
+
+                int newX = MyPictureBox_2.Location.X + deltaX;
+                int newY = MyPictureBox_2.Location.Y + deltaY;
+
+                MyPictureBox_2.Location = new Point(newX, newY);
+            }
+        }
+
+        public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+
+        private void nextImage ()
+        {
+            if (MyPosition < MyMaximum - 1)
+            {
+                
+                MyPosition++;
+                MyLabelScore.Text = score[MyPosition].ToString();
+                MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                MyLabelPosition.Text = MyPosition.ToString();
+                MyProgressbar.Value = MyPosition;
+            }
+            else {
+                Console.Beep();
+            }
+        }
+
+        private void next100Images()
+        {
+            if (MyPosition < MyMaximum - 100)
+            {
+
+                MyPosition=MyPosition+100;
+                MyLabelScore.Text = score[MyPosition].ToString();
+                MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                MyLabelPosition.Text = MyPosition.ToString();
+                MyProgressbar.Value = MyPosition;
+            }
+            else
+            {
+                MyPosition = MyMaximum-1;
+                MyLabelScore.Text = score[MyPosition].ToString();
+                MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                MyLabelPosition.Text = MyPosition.ToString();
+                MyProgressbar.Value = MyPosition;
+                Console.Beep();
+            }
+        }
+
+        private void previousImage()
+        {
+            if (MyPosition > 0)
+            {
+                
+                MyPosition--;
+                MyLabelScore.Text = score[MyPosition].ToString();
+                MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                MyLabelPosition.Text = MyPosition.ToString();
+                MyProgressbar.Value = MyPosition;
+ 
+            }
+            else { Console.Beep(); }
+        }
+
+        private void previous100Images()
+        {
+            if (MyPosition > 100)
+            {
+
+                MyPosition= MyPosition -100;
+                MyLabelScore.Text = score[MyPosition].ToString();
+                MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                MyLabelPosition.Text = MyPosition.ToString();
+                MyProgressbar.Value = MyPosition;
+
+            }
+
+            else {
+                MyPosition = 0;
+                MyLabelScore.Text = score[MyPosition].ToString();
+                MyPictureBox_2.Image = Image.FromFile(files[MyPosition]);
+                MyLabelFilename.Text = Path.GetFileNameWithoutExtension(files[MyPosition]);
+                MyLabelPosition.Text = MyPosition.ToString();
+                MyProgressbar.Value = MyPosition;
+                Console.Beep(); }
+        }
+
+        private void writeResults()
+        {
+            if (score != null && !Array.Exists(score, element => element.Equals(0)))
+            {
+                string outfile = Path.GetFileNameWithoutExtension(files[0]).Substring(0, 5);
+                string outfolder = Path.GetDirectoryName(files[0]);
+                mytable = new string[files.Length];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    mytable[i] = Path.GetFileNameWithoutExtension(files[i]) + ";" + score[i].ToString();
+                }
+                File.WriteAllLines(outfolder + "\\" + outfile + ".txt", mytable, Encoding.UTF8);
+                MessageBox.Show("Results successfully written to: " + outfolder + "\\"+ outfile + ".txt.");
+            }
+            else
+            {
+                if (score != null)
+                { 
+                int x = 0;
+                for (int i = 0; i < score.Length - 1; i++)
+                {
+                    if (score[i] == 0)
+                    {
+                        x++;
+                    }
+                }
+
+                Console.Beep();
+                MessageBox.Show(x.ToString()+ " Pictures have not been rated. First missing value is at index: "+ Array.IndexOf(score,0).ToString());
+                }
+            }
+        }
+
+    }
+}
